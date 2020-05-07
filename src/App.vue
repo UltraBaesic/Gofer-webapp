ce69e30193734f6e9c5b3975e87473f0938a71cf
Afolabi Opakunle <opakunleafolabi@gmail.com>
2020-05-07 01:38:20 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,24 @@ export default {
   background: #eef6f9;
   width: 100vw;
 }
+
+::-webkit-scrollbar {
+  width: 6px;
+}
+
+::-webkit-scrollbar-track {
+  background: whitesmoke;
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #0084b9a2;
+  border-radius: 5px;
+  display: none;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  display: block;
+}
+
 </style>
